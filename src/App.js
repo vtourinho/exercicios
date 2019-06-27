299c66ebccde3b38558c395dccf2513ffc538221
vtourinho <vitor.tourinho@gmail.com>
2019-06-26 23:42:04 -0300

    MegaSena

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Simples from './Componentes/Simples'
-import { Inverter } from "./Componentes/Multi"
+import { Inverter, MegaSena } from "./Componentes/Multi"
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -28,6 +28,7 @@ export default class App extends Component {
         <Text style={styles.instructions}>{instructions}</Text>
         <Text style={styles.welcome}>Agora vai com Git!</Text>
         <Inverter texto='React Nativo!' />
+        <MegaSena numeros={6} />
       </View>
     );
   }
